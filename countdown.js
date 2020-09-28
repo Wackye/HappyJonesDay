449b710edbd26bb458ffcd954cbf281e187fbefb
Wackye <j4170149@gmail.com>
2020-09-28 13:16:07 +0800

    update

--- countdown.js
+++ countdown.js
@@ -14,13 +14,13 @@ function draw(){
     // 現在時間:hours
     var timeRemain = getTimeRemaining('2020-09-29T01:00:00');
     fill(240);
-     rect(width/2 - 132,height/2-6,80,80);
+     rect(width/2 - 140,height/2-6,80,80);
      rect(width/2,height/2-6,80,80);
-     rect(width/2 + 132,height/2-6,80,80);
+     rect(width/2 + 140,height/2-6,80,80);
 
     fill(0);
-    text(padLeft(timeRemain.hours,2),width/2 - 132 -fontSize /2, height/2 + fontSize /2 - 15);
+    text(padLeft(timeRemain.hours,2),width/2 - 136 -fontSize /2, height/2 + fontSize /2 - 15);
     text(padLeft(timeRemain.minutes,2),width/2 -fontSize /2, height/2 + fontSize /2- 15);
-    text(padLeft(timeRemain.seconds,2),width/2 + 132-fontSize /2, height/2 + fontSize /2- 15);
+    text(padLeft(timeRemain.seconds,2),width/2 + 140-fontSize /2, height/2 + fontSize /2- 15);
 
 }
